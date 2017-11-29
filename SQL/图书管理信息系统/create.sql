b1a92f769066fe2ab4e0a0908da807fd709890dd
LuMing <784315443@qq.com>
2017-11-29 12:00:00 +0800

    updata libiary manage system SQL.

--- SQL/图书管理信息系统/create.sql
+++ SQL/图书管理信息系统/create.sql
@@ -23,17 +23,17 @@ use 图书管理信息系统
 
 create table book_cate
 (
-类别编号 int primary key,
-类别名称 char(10),
+类别编号 char(50) primary key,
+类别名称 char(20),
 关键词 char(50),
 备注信息 char(50)
 )
 
 create table book
 (
-书籍编号 int primary key,
+书籍编号 char(50) primary key,
 书籍名称 char(50), 
-书籍类别  int foreign key references book_cate(类别编号), 
+书籍类别  char(50) foreign key references book_cate(类别编号), 
 作者名称 char(50) ,
 出版社名称 char(50),
 出版日期 datetime,
@@ -44,7 +44,7 @@ create table book
 )
 create table reader_cate
 (
-种类编号 int primary key,
+种类编号 char(50) primary key,
 种类名称 char(20),
 借书数量 int ,
 借书期限 int,
@@ -53,9 +53,9 @@ create table reader_cate
 
 create table reader
 (
-读者编号 int primary key,
+读者编号 char(50) primary key,
 读者姓名 char(10),
-读者种类 int foreign key references reader_cate(种类编号),
+读者种类 char(50) foreign key references reader_cate(种类编号),
 读者性别 char (4),
 工作单位 char(50),
 家庭地址 char(50),
@@ -67,9 +67,9 @@ create table reader
 
 create table borrow_relation
 (
-借阅编号 int primary key,
-读者编号 int foreign key references reader(读者编号),
-书籍编号 int foreign key references book(书籍编号),
+借阅编号 char(50) primary key,
+读者编号 char(50) foreign key references reader(读者编号),
+书籍编号 char(50) foreign key references book(书籍编号),
 借书日期 datetime not null,
 还书日期 datetime,
 备注信息 char(50)
