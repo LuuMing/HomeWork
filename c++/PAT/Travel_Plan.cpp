2d2e9d132fd32936c34ed6b5475a0bfc4661c636
LuMing <784315443@qq.com>
2018-01-29 22:45:22 +0800

    Travel_Plan version 4, almost done, but run out of time. Needs simplfy.

--- c++/PAT/Travel_Plan.cpp
+++ c++/PAT/Travel_Plan.cpp
@@ -1,29 +1,30 @@
 #include<iostream>
+#include<cstdio>
 #include<list>
-#define inf 0x3f3f3f3f
+#include<set>
+#include<algorithm>
+#define inf 99999
 using namespace std;
 int ** dist_matrix = NULL;
 int ** cost_matrix = NULL;
+list<int> * pre = NULL; 
 int N = 0;
 int M = 0;
 int S = 0;
 int D = 0;
-
-class Path
+class List:public list<int>
 {
-	private:
-		list<int> p;
 	public:
 		int total_distance()
 		{
-			if (p.size() == 1)
+			if (this->size() == 1)
 			{
 				return 0;
 			}
 			int sum = 0;
-			list<int>::iterator e = p.end();
+			list<int>::iterator e = this->end();
 			e--;		
-			for(list<int>::iterator i = p.begin(); i != e;)
+			for(list<int>::iterator i = this->begin(); i != e;)
 			{
 				sum = sum + dist_matrix[*i][*(++i)];
 			}
@@ -31,81 +32,68 @@ class Path
 		}
 		int total_cost()
 		{
-			if (p.size() == 1)
+			if (this->size() == 1)
 			{
 				return 0;
 			}
 			int sum = 0;
-			list<int>::iterator e = p.end();
+			list<int>::iterator e = this->end();
 			e--;
-			for(list<int>::iterator i = p.begin(); i != e;)
+			for(list<int>::iterator i = this->begin(); i != e;)
 			{
 				sum = sum + cost_matrix[*i][*(++i)];
 			}
 			return sum;
 		}
-		Path(int i)
-		{
-			p.push_back(i);
-		}
-		void add(int n)
-		{
-			p.push_back(n);
-		}
-		void add(Path &ii)
-		{
-			for(list<int>::iterator i = ii.p.begin(); i!=ii.p.end();i++)
-			{
-				p.push_back(*i);
-			}
-		}
-		Path(const Path &ii)
-		{
-			p = ii.p;
-		}
-		void pop()
-		{
-			p.pop_back();
-		}
-		void output()
-		{
-			for(list<int>::iterator i = p.begin(); i != p.end(); i++)
-			{
-				cout<<*i<<' ';
-			}
-			cout<<this->total_distance()<<' ';
-			cout<<this->total_cost();
-		}
-		bool operator < ( Path &i)
-		{
-			return this->total_cost() < i.total_cost();
-		}
-		void debug()
-		{
-			cout<<"****************"<<endl;
-			for(list<int>::iterator i = p.begin(); i != p.end(); i++)
-			{
-				cout<<*i<<' '<<"->";
-			}
-			cout<<endl<<"****************"<<endl;
-		}
 };
-void view_map(int **& m)
+
+list<int>* pathfind(int s)   
 {
-	
-	cout<<"*****************************"<<endl;
-	for(int i = 0; i< N; i++)
+	int *D = new int [N];               //中间向量D 
+	list<int>* Path = new list<int>[N];
+	set<int> Set;
+	Set.insert(s);                      //初始顶点加入集合 
+	for(int i = 0; i < N;i++)
 	{
-		for(int j = 0; j< N;j++)
+		D[i] = dist_matrix[s][i];      //生成中间向量
+		Path[i].clear();
+	}                                   
+	while( Set.size() < N)              //not all node in Set, DO!
+	{
+		int num = 0;
+		int dist = inf;
+		for(int i = 0; i < N; i++)
 		{
-			cout<<m[i][j]<<"   ";
-		}
-		cout<<endl;
+			if( Set.find(i)==Set.end() && D[i] < dist) //找出与顶点相连最短点,且此点未加入集合 
+			{                          
+				num = i;
+				dist = D[i];                           //记录编号num 
+			}
+		}                                
+		for(int i = 0; i < N; i++)
+		{
+			int temp = dist + dist_matrix[num][i];
+			if( Set.find(i)==Set.end() && temp < D[i])                    
+			{
+				D[i] = temp;                                   
+				Path[i].clear();
+				Path[i].push_back(num);	                                         
+			}
+			else if( Set.find(i)==Set.end() && temp == D[i])                    
+			{
+				D[i] = temp;                                  
+				Path[i].push_back(num);	                                         
+			}                             
+		}                           
+		Set.insert(num);
 	}
+	return Path;                       //D[i]即为顶点到i点的最短距离 	                                      
 }
+
+
 void init_matrix(int**& matrix,int N)
 {
-	matrix  = new int*[N];
+	matrix = new int*[N];
 	for (int i = 0; i < N; i++)
 	{
 		matrix[i] = new int[N];
@@ -114,79 +102,62 @@ void init_matrix(int**& matrix,int N)
 	{
 		for(int j = 0; j < N; j++)
 		{
+			if(i==j)
+			{
+				matrix[i][j] = 0;
+				continue;
+			}
 			matrix[i][j] = inf;
 		}
 	}
 }
-void matrix_copy(int **& A,int **&B,int N)
-{
-	for(int i = 0; i < N;i++)
+//void view_map(int **& m)
+//{
+//	cout<<"*****************************"<<endl;
+//	for(int i = 0; i< N; i++)
+//	{
+//		for(int j = 0; j< N;j++)
+//		{
+//			if(m[i][j]==inf)
+//			{
+//				cout<<"∞"<<"    ";
+//			}
+//			else
+//				cout<<m[i][j]<<"    ";
+//		}
+//		cout<<endl;
+//	}
+//}
+list<List> find_all_path( int end )
+{	
+	list<List> all_path;
+	for(list<int>::iterator i = pre[end].begin(); i != pre[end].end(); i++)          //第i个节点的所有前驱点 
 	{
-		for(int j = 0; j < N ;j++)
+		if ( (*i) == end )                                            //如果i的前驱节点中有end 
 		{
-			A[i][j] = B[i][j];
+			List direct_path;
+			direct_path.push_back(*i);
+			direct_path.push_back(S);
+			all_path.push_back(direct_path);          
+			return all_path;                                          //生成一条路   start<-i(==end) 
 		}
-	}
-}
-
-Path find_path(int s,int e)
-{	if(s == e)
-	{
-		Path p(s);
-		return p;
-	}
-	int **temp_dist_matrix;
-	int **temp_cost_matrix;
-	list<Path> short_way;
-	init_matrix(temp_dist_matrix,N);
-	init_matrix(temp_cost_matrix,N);
-	matrix_copy(temp_dist_matrix,dist_matrix,N);
-	matrix_copy(temp_cost_matrix,dist_matrix,N);
-	
-	for(int i = 0 ; i < N; i++)
-	{
-		for(int j = 0 ;j < N; j++)
-		{
-			Path p(i);
-			for(int k = 0; k < N; k++)
+		else
+		{                                                    //对于其他i,先加入end，再找i是否有子路回到起点 
+			list<List> another_path = find_all_path(*i);      //                         start<-i的子路<-end
+			for( list<List>::iterator i = another_path.begin(); i != another_path.end();i++)
 			{
-				if( temp_dist_matrix[i][j] > temp_dist_matrix[i][k] + temp_dist_matrix[k][j] )
-				{
-					p.add(k);
-					temp_dist_matrix[i][j] = temp_dist_matrix[i][j] + temp_dist_matrix[k][j];
-				//	continue;
-				}
-				
-//				if( temp_dist_matrix[i][j] == temp_dist_matrix[i][k] + temp_dist_matrix[j][k])
-//				{                                                                 
-//					Path another_way(p);                     //若有次最短路则添加至list 
-//					another_way.pop();
-//					another_way.add(k);
-//					short_way.push_back(another_way);
-//				}			
+				List sub_path;
+				sub_path.push_back(end);
+				sub_path.insert(sub_path.end(),(*i).begin(),(*i).end());           //所有可回到起点的子路 
+				all_path.push_back(sub_path);
 			}
-			p.add(j);
-			short_way.push_back(p);
 		}
-	} 
-
-	for(list<Path>::iterator i = short_way.begin(); i!=short_way.end();i++)
-	{
-		(*i).debug();
 	}
-	short_way.sort();                //重载<让cost对最短路的排序
-	//////////////////////
-//	cout<<"中间矩阵是:"<<endl;
-//	view_map(temp_dist_matrix);
-//	cout<<"*********";
-//	///////////////////
-	
-	return *(short_way.begin());    //返回第一条即为cost最小的最短路 
+	return all_path;
 }
 
 int main()
 {
-	
 	cin >> N;
 	cin >> M;
 	cin >> S;
@@ -201,9 +172,25 @@ int main()
 		dist_matrix[destination][start] = dist;
 		cost_matrix[destination][start] = cost;
 		cost_matrix[start][destination] = cost;
+	}  
+	pre = pathfind(S);
+	list<List> all_path = find_all_path(D);
+	auto best_path = all_path.begin();
+	for(auto i = all_path.begin(); i!= all_path.end();i++)
+	{
+		if(i->total_distance() <= best_path->total_distance())
+			best_path = i;                                       //最短 
 	}
-	Path i = find_path(S,D);
-	i.output();
+	
+	for(auto i = all_path.begin(); i!=all_path.end();i++)
+	{
+		if(i->total_distance() == best_path->total_distance() && i->total_cost()<best_path->total_cost())
+			best_path = i;                                       //最低cost 
+	}
+	reverse(best_path->begin(),best_path->end());
+	for(auto i = (*best_path).begin(); i!= (*best_path).end();i++)
+	{
+		cout<<*i<<' ';
+	}
+	cout<<best_path->total_distance()<<' '<<best_path->total_cost();
 }
-
-
