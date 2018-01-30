f6f4a79c126986a4b841dcb62ebe576f6c4e1684
LuMing <784315443@qq.com>
2018-01-30 14:07:31 +0800

    Travel_Plan version 5, Success! keywords:dijkstra, BFS.

--- c++/PAT/Travel_Plan.cpp
+++ c++/PAT/Travel_Plan.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
-#include<cstdio>
 #include<list>
 #include<set>
 #include<algorithm>
 #define inf 99999
 using namespace std;
-int ** dist_matrix = NULL;
-int ** cost_matrix = NULL;
-list<int> * pre = NULL; 
+int ** dist_matrix = NULL;                //距离邻接矩阵 
+int ** cost_matrix = NULL;                //cost邻接矩阵 
+list<int> * pre = NULL;                   //前驱结点 
 int N = 0;
 int M = 0;
 int S = 0;
 int D = 0;
-class List:public list<int>
+class List:public list<int>              //list派生类 代表一条路径 
 {
 	public:
 		int total_distance()
@@ -47,21 +46,25 @@ class List:public list<int>
 		}
 };
 
-list<int>* pathfind(int s)   
+list<int>* pathfind(int s)               //Dijkstra算法，返回前驱节点容器 
 {
-	int *D = new int [N];               //中间向量D 
-	list<int>* Path = new list<int>[N];
+	int *D = new int [N];                //中间向量D 
+	list<int>* Path = new list<int>[N];  //记录前驱节点 
 	set<int> Set;
 	Set.insert(s);                      //初始顶点加入集合 
 	for(int i = 0; i < N;i++)
 	{
 		D[i] = dist_matrix[s][i];      //生成中间向量
 		Path[i].clear();
-	}                                   
-	while( Set.size() < N)              //not all node in Set, DO!
+	}
+	int Set_size = 0;   
+    //注意：不能存在孤立点，否则死循环
+	//解决办法，使用一个值来判断Set大小是否发生变化，当添加不进去孤立点时退出循环 
+	while( Set.size() != Set_size)             
 	{
 		int num = 0;
 		int dist = inf;
+		Set_size = Set.size();
 		for(int i = 0; i < N; i++)
 		{
 			if( Set.find(i)==Set.end() && D[i] < dist) //找出与顶点相连最短点,且此点未加入集合 
@@ -75,23 +78,23 @@ list<int>* pathfind(int s)
 			int temp = dist + dist_matrix[num][i];
 			if( Set.find(i)==Set.end() && temp < D[i])                    
 			{
-				D[i] = temp;                                   
-				Path[i].clear();
+				D[i] = temp;                                    
+				Path[i].clear();                       //可进行松弛的点 
 				Path[i].push_back(num);	                                         
 			}
 			else if( Set.find(i)==Set.end() && temp == D[i])                    
 			{
-				D[i] = temp;                                  
-				Path[i].push_back(num);	                                         
+				D[i] = temp;                           //距离相等的点       
+				Path[i].push_back(num);                       	                                         
 			}                             
 		}                           
 		Set.insert(num);
 	}
-	return Path;                       //D[i]即为顶点到i点的最短距离 	                                      
+	return Path;                                      //Path[i]代表从起始点到i点的所有前驱节点(包含相等)                      
 }
 
 
-void init_matrix(int**& matrix,int N)
+void init_matrix(int**& matrix,int N)        //动态分配不浪费 
 {
 	matrix = new int*[N];
 	for (int i = 0; i < N; i++)
@@ -111,24 +114,24 @@ void init_matrix(int**& matrix,int N)
 		}
 	}
 }
-//void view_map(int **& m)
-//{
-//	cout<<"*****************************"<<endl;
-//	for(int i = 0; i< N; i++)
-//	{
-//		for(int j = 0; j< N;j++)
-//		{
-//			if(m[i][j]==inf)
-//			{
-//				cout<<"∞"<<"    ";
-//			}
-//			else
-//				cout<<m[i][j]<<"    ";
-//		}
-//		cout<<endl;
-//	}
-//}
-list<List> find_all_path( int end )
+void view_map(int **& m)           //debug用，查看矩阵 
+{
+	cout<<"*****************************"<<endl;
+	for(int i = 0; i< N; i++)
+	{
+		for(int j = 0; j< N;j++)
+		{
+			if(m[i][j]==inf)
+			{
+				cout<<"∞"<<"    ";
+			}
+			else
+				cout<<m[i][j]<<"    ";
+		}
+		cout<<endl;
+	}
+}
+list<List> find_all_path( int end )                        // DFS,使用前驱结点从end找出到start的所有路 
 {	
 	list<List> all_path;
 	for(list<int>::iterator i = pre[end].begin(); i != pre[end].end(); i++)          //第i个节点的所有前驱点 
@@ -143,7 +146,7 @@ list<List> find_all_path( int end )
 		}
 		else
 		{                                                    //对于其他i,先加入end，再找i是否有子路回到起点 
-			list<List> another_path = find_all_path(*i);      //                         start<-i的子路<-end
+			list<List> another_path = find_all_path(*i);      //                 start<-i的子路<-end
 			for( list<List>::iterator i = another_path.begin(); i != another_path.end();i++)
 			{
 				List sub_path;
@@ -158,6 +161,7 @@ list<List> find_all_path( int end )
 
 int main()
 {
+	/* 初始化*/
 	cin >> N;
 	cin >> M;
 	cin >> S;
@@ -172,25 +176,38 @@ int main()
 		dist_matrix[destination][start] = dist;
 		cost_matrix[destination][start] = cost;
 		cost_matrix[start][destination] = cost;
-	}  
+	}
+	/* debug用 查看矩阵 */
+	//	view_map(dist_matrix); 
+	//	view_map(cost_matrix);
+	 
+	/*找出到start的所有前驱节点*/
 	pre = pathfind(S);
+	/*找出所有路径 */
 	list<List> all_path = find_all_path(D);
-	auto best_path = all_path.begin();
-	for(auto i = all_path.begin(); i!= all_path.end();i++)
+	/* 输出 */
+	if(all_path.empty())
+	{
+		return 0;                          
+	}	
+	list<List>::iterator best_path = all_path.begin();    //用于记录最好的一条路 
+	
+	for(list<List>::iterator i = all_path.begin(); i!= all_path.end();i++)
 	{
 		if(i->total_distance() <= best_path->total_distance())
 			best_path = i;                                       //最短 
 	}
-	
-	for(auto i = all_path.begin(); i!=all_path.end();i++)
+	for(list<List>::iterator i = all_path.begin(); i!=all_path.end();i++)
 	{
 		if(i->total_distance() == best_path->total_distance() && i->total_cost()<best_path->total_cost())
 			best_path = i;                                       //最低cost 
 	}
-	reverse(best_path->begin(),best_path->end());
-	for(auto i = (*best_path).begin(); i!= (*best_path).end();i++)
+	
+	reverse(best_path->begin(),best_path->end());               //翻转path,从start->end 
+	for(list<int>::iterator i = (*best_path).begin(); i!= (*best_path).end();i++)
 	{
 		cout<<*i<<' ';
 	}
 	cout<<best_path->total_distance()<<' '<<best_path->total_cost();
+	return 0;
 }
