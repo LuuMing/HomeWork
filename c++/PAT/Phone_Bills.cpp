2a7711920934d53f00f7965a9b216b2b168d7b94
LuMing <784315443@qq.com>
2018-02-20 19:13:16 +0800

    PAT 1016.Phone Bills(25). Given the call log, to compute vaild log's cost and total time. sort record by time. Find pair of vaild call log. Compute the cost, it charges variously by time. P.S. Note the method of compute cost. Before version faild with out of index. P.S. If container in sturct, better use pointer. rate:★★★☆☆ time:★★★★☆

--- c++/PAT/Phone_Bills.cpp
+++ c++/PAT/Phone_Bills.cpp
@@ -1,140 +1,91 @@
 #include<iostream>
-#include<cstdio>
 #include<vector>
-#include<string>
 #include<map>
 #include<algorithm>
 using namespace std;
-vector<int> charge;
 int day_charge;
-struct Time
+vector<int> charge;
+struct record
 {
-	int month;
-	int dd;
-	int hh;
-	int mm;
-	Time (int a,int b,int c, int d)
-	{
-		month = a;
-		dd = b;
-		hh = c;
-		mm = d;
-	}
-	Time(){}
+	string ID,line;
+	int month,dd,hh,mm;
+	int total;
 };
-bool cmp  (const Time &T1,const Time & T2)
+string get_time(record r)
 {
-	string t1,t2;
- 	t1+= to_string(T1.month);
-	t1+= to_string(T1.dd);
-	t1+= to_string(T1.hh);
-	t1+= to_string(T1.mm);
-	t2 += to_string(T2.month);
-	t2 += to_string(T2.dd);
-	t2 += to_string(T2.hh);
-	t2 += to_string(T2.mm);
-	return t1 < t2;
+	char buffer[12];
+	sprintf(buffer,"%02d:%02d:%02d:%02d",r.month,r.dd,r.hh,r.mm);
+	return string(buffer);
 }
-double compute_cost(Time s,Time e)
+bool cmp(const record &c1,const record &c2 )
 {
-	
+	if(c1.ID!=c2.ID)
+	{
+		return c1.ID < c2.ID;
+	}
+	else
+	{
+		return get_time(c1) < get_time(c2);
+	}
+}
+double compute_cost(const record & s,const record & e)
+{
 	double cost = 0;
-	cost -= s.dd * day_charge;
-	cost -= s.mm * charge[s.hh];
-	cost += e.dd * day_charge;
-	cost += e.mm * charge[e.hh];
-	for(int i = s.hh; i !=e.hh; i++)
+        cost -= s.dd * day_charge;
+        cost -= s.mm * charge[s.hh];
+        cost += e.dd * day_charge;
+        cost += e.mm * charge[e.hh];
+        for(int i = 0; i != s.hh; i++)
+        {
+                cost -= 60 * charge[i];
+        }
+	for(int i = 0; i != e.hh;i++)
 	{
-		cost += 60 * charge[i];
+		cost += 60*charge[i];
 	}
-	return cost/100;
-}
-int compute_time(Time s,Time e)
-{
-	return (e.dd-s.dd)*1440 + (e.hh-s.hh)*60+(e.mm-s.mm);
-}
-struct person
-{
-	vector<Time> on_line;
-	vector<Time> off_line;
-	vector<pair<Time,Time>>  record;
-	int month;
-	double total_cost()
-	{
-		double total = 0;
-		for(auto &i:this->record)
-		{
-			total += compute_cost(i.first,i.second);
-		}
-		return total;
-	}
+        return cost/100;
 	
-};
-
-map <string,person> p;
-int N;
+	cout << endl;
+}
 int main()
 {
+	int N = 0;
+	map<string,vector<pair<record,record>>> round;
 	charge.resize(24);
 	day_charge = 0;
-	for(int i = 0; i < 24; i++)
+	for(int i = 0; i < 24;i++)
 	{
-		scanf("%d",&charge[i]);
+		cin >> charge[i];
 		day_charge += charge[i];
 	}
 	day_charge *= 60;
-	cin >> N;
-	for(int i = 0; i < N; i++)
+	cin  >> N;
+	vector<record> records(N);
+	for(int i = 0; i < N;i++)
 	{
-		string id,line;
-		Time time;
-		cin >> id;
-		scanf("%d:%d:%d:%d ",&time.month,&time.dd,&time.hh,&time.mm);
-		cin >> line;
-		if(line=="on-line")
-		{
-			p[id].on_line.push_back(time);
-		}
-		else
-		{
-			p[id].off_line.push_back(time);
-		}
-		p[id].month = time.month;
+		cin >> records[i].ID;
+		scanf("%d:%d:%d:%d",&records[i].month,&records[i].dd,&records[i].hh,&records[i].mm);
+		cin >> records[i].line;
+		records[i].total = records[i].dd * 24 *60+records[i].hh*60+records[i].mm;
 	}
-	for(auto &i:p)
+	sort(records.begin(),records.end(),cmp);
+	for(int i = 1; i < records.size(); i++)
 	{
-		sort(i.second.on_line.begin(),i.second.on_line.end(),cmp);
-		sort(i.second.off_line.begin(),i.second.off_line.end(),cmp);
-	}
-	for(auto &i:p)
-	{
-		while((!i.second.on_line.empty()) && (!i.second.off_line.empty()))
+		if(records[i].ID == records[i-1].ID && records[i].line == "off-line" && records[i-1].line == "on-line")
 		{
-			if(cmp(*(i.second.on_line.end()-1), *(i.second.off_line.end()-1)))
-			{
-				i.second.record.push_back(make_pair(*(i.second.on_line.end()-1),*(i.second.off_line.end()-1)));
-				i.second.on_line.erase(i.second.on_line.end()-1);
-				i.second.off_line.erase(i.second.off_line.end()-1);
-			}
-			else
-			{
-				i.second.on_line.erase(i.second.on_line.end()-1);
-			}		
+			round[records[i].ID].push_back(make_pair(records[i-1],records[i]));
 		}
-					
 	}
-	for(auto &i:p)
+	for(auto &i:round)
 	{
-		cout << i.first<<' ';
-		printf("%02d\n",i.second.month);
-		for(auto j = i.second.record.begin(); j!= i.second.record.end();j++)
-		{
-			printf("%02d:%02d:%02d %02d:%02d:%02d ",j->first.dd,j->first.hh,j->first.mm,j->second.dd,j->second.hh,j->second.mm);
-			cout << compute_time(j->first,j->second) << " $";
-			printf("%.2f\n",compute_cost(j->first,j->second));
-		}
-		printf( "Total amount: $%.2f\n",i.second.total_cost());	
-	}
-	
-}
+		double total_cost = 0;
+		printf("%s %02d\n",i.first.c_str(),i.second[0].first.month);
+                for(auto j:i.second)
+                {
+			total_cost += compute_cost(j.first,j.second);
+                        printf("%02d:%02d:%02d %02d:%02d:%02d %d $%.2f\n",j.first.dd,j.first.hh,j.first.mm,j.second.dd,j.second.hh,j.second.mm,j.second.total-j.first.total,compute_cost(j.first,j.second));
+                }
+                printf("Total amount: $%.2f\n",total_cost);
 
+	}
+}
