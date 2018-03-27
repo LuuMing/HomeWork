da0fa25489c7ac482a70cbc836bdec3472ef0112
LuMing <784315443@qq.com>
2018-03-27 20:20:42 +0800

    PAT.Tree Traversals Again(25). [Twice] hard:★★★☆☆ time:★★☆☆☆ Speed up, optimal output.

--- c++/PAT/Tree_Traversals_Again.cpp
+++ c++/PAT/Tree_Traversals_Again.cpp
@@ -1,59 +1,66 @@
 #include<iostream>
-#include<sstream>
+#include<cstdio>
 using namespace std;
-stringstream  ss;
 struct node
 {
-	int num ;
-	node*left = NULL;
-	node* right = NULL;
+	int data;
+	node * left;
+	node * right;
 };
 int N;
 int times = 0;
+int out_times = 0;
+void output(node * root)
+{
+	if(root)
+	{
+		if(root->left)
+		{
+			output(root->left);
+		}
+		if(root->right)
+		{
+			output(root->right);
+		}
+		if(out_times != N-1)
+		{
+			cout << root->data << ' ';
+			out_times++;
+		}
+		else
+		{
+			cout << root->data;
+		}
+	}
+}
 node * build()
 {
-	node * r = NULL;
-	if(times < 2*N)	
+	node * root = NULL;
+	char query[5];
+	int t;
+	if (times < 2*N)
 	{
-		string tem;
-		cin >> tem;
-		if(tem[1] == 'u')
+		scanf("%s",query);
+		if(query[1]=='u')
 		{
-			r = new node;
-			cin >> r -> num;
+			cin >> t;
+			root = new node;
+			root->data = t;
 			times++;
 		}
 		else
 		{
 			times++;
-			return NULL;
+			return root;
 		}
-	        r->left = build();
-	        r->right =build();
+		root -> left = build();
+		root -> right = build();
 	}
-	return r;
-}
-void post(node * root)
-{
-	if(root -> left != NULL)
-	{
-		post(root ->left);
-	}
-	if(root -> right != NULL)
-	{
-		post(root -> right);
-	}
-	if(root != NULL)
-	{
-		ss << root->num << ' ';
-	}
+	return root;
 }
 int main()
 {
 	cin >> N;
-	node* r = build();
-	post(r);
-	string s = ss.str();
-	s.erase(s.end()-1);
-	cout << s;
+	node * root = build();
+	output(root);
 }
