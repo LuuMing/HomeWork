3f7562e19b3c6b24e54d36cbf27716c64ec5d369
LuMing <784315443@qq.com>
2018-04-16 16:48:05 +0800

    PAT 1021.Deepest Root(25). Find the vertex of edge of Deepest path in a graph. Use union-find to determine whether if graph is connected. If connected graph, use just 2 times DFS could find all vertex: 1.DFS: random chose vertex as start in DFS, then insert the max length's vertex into set.(it could be missing some vertex) 2.DFS: chose one of the vertex in set as the start in DFS, repeat 1. Then, we will find all. hard:★★★★☆ time:★★★★★ thanks to: http://blog.csdn.net/sinat_29278271

--- c++/PAT/Deepest_Root.cpp
+++ c++/PAT/Deepest_Root.cpp
@@ -1,65 +1,79 @@
 #include<iostream>
+#include<algorithm>
+#include<set>
 using namespace std;
 #define MAX 10001
-int adj[MAX][MAX];
+vector<int> adj[MAX];
+int visited[MAX];
 int N;
-bool visited[MAX];
+int max_length;
 int length[MAX];
-void DFS(int v,int root,int level,bool count = true)
+set<int> s;
+int find(int a,int * pre)
 {
-	visited[v] = true;
-	if(level > length[root] && count)
+	if(pre[a] == a)
 	{
-		length[root] = level;
-	}
-	for(int i = 1; i <= N;i++)
-	{
-		if(adj[v][i] && !visited[i])
-		{
-			 DFS(i,root,level+1,true);
-		}
-	}
-}
-int list()
-{
-	int count = 0;
-	for(int i = 1; i<= N;i++) visited[i] = false;
-	for(int i = 1; i<= N;i++)
-	{
-		if(!visited[i])
-		{
-			DFS(i,visited[i],0,false);
-			count ++;
-		}
-	}
-	return count;
-}
-int main()
-{
-	cin >> N;	
-	for(int i = 1; i < N; i++)
-	{
-		int a,b; cin >> a >> b;
-		adj[a][b] = adj[b][a] = 1;
-	}
-	for(int i = 1; i <= N;i++)
-	{
-		DFS(i,i,1);
-		for(int i = 1; i <= N;i++) visited[i] = false;
-	}
-	int components = list();
-	if(components == 1)
-	{
-		int max = -1;
-		for(int i = 1; i <= N;i++) if( length[i] > max ) max = length[i];
-		for(int i =1; i <= N;i++)  
-		{
-			if(length[i] == max) 
-				cout << i << endl;
-		}
+		return a;
 	}
 	else
 	{
-		cout << "Error: " << components <<" components"; 
+		pre[a] = find(pre[a],pre);
+		return pre[a];
 	}
+}
+void merge(int a,int b,int * pre)
+{
+	int set_A = find(a,pre);
+	int set_B = find(b,pre);
+	if(set_A == set_B) return;
+	else 
+		pre[set_A] = set_B;
+}
+void DFS(int v, int l)
+{
+	visited[v] = true;
+	length[v] = l;
+	max_length = max(l,max_length);
+	for(int i = 0; i < adj[v].size();i++)
+	{
+		if( !visited[adj[v][i]])
+			DFS(adj[v][i],l+1);
+	}
+}
+int main()
+{
+	int * pre = (int * )malloc(sizeof(int) * MAX);
+	int components = 0;
+	cin >> N; for(int i = 1; i <= N;i++) pre[i] = i;
+	for(int i = 1; i< N;i++)
+	{
+		int a,b; cin >> a >> b;
+		merge(a,b,pre);
+		adj[a].push_back(b);
+		adj[b].push_back(a);
+	}
+	for(int i = 1; i<= N;i++)
+	{
+		if(pre[i] == i) components++;
+	}
+	if(components==1)
+	{
+		DFS(1,1);
+		for(int i = 1; i <= N;i++)
+		{
+			visited[i]  = false;
+			if(length[i] == max_length)  s.insert(i);
+		}
+		DFS(*s.begin(),1);
+		for(int i = 1; i <= N;i++)
+		{
+			if(length[i] == max_length) s.insert(i);
+		}
+		for(auto &i:s) cout << i << endl;
+	}
+	else
+	{
+		cout << "Error: "<<components<<" components";
+	}
+	return 0;
 }
