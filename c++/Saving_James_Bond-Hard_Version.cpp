de0ce2a32ca9bdd337c5a3074d0d8c05705ad3a8
LuMing <784315443@qq.com>
2018-04-25 19:19:27 +0800

    Pintia Saving James Bond - Hard Version(30). Unweighted graph's short path. By modify BFS. Notice: bool connected(int v1,int v1) { 	if(dx*dx + dy*dy <= D)    //!!! less than, not less. 		return true; 	return false; }

--- c++/Saving_James_Bond-Hard_Version.cpp
+++ c++/Saving_James_Bond-Hard_Version.cpp
@@ -1,132 +1,118 @@
 #include<iostream>
-#include<math.h>
 #include<vector>
+#include<queue>
 #include<algorithm>
-using namespace std;
 #define MAX 101
+using namespace std;
 struct node
 {
-	int x,y;
-	friend bool operator < (const node & n1,const node & n2)
-	{
-		int p1  = n1.x * n1.x + n1.y*n1.y;
-		int p2 = n2.x * n2.x + n2.y * n2.y;
-		return p1 < p2;
-	}
+	int x;int y;
 };
 vector<node> p;
+int path[MAX];
 int N;
 int D;
-int dist[MAX];
-int path[MAX];
-vector<int> r;
-vector<int> Path;
-bool connect(int v1,int v2)
+bool cmp(const int & v1, const int & v2)
 {
-	double dx =  p[v1].x - p[v2].x;
-	double dy = p[v1].y - p[v2].y;
-	if(dx*dx + dy*dy < D * D)
-	{
+	int d1 = p[v1].x * p[v1].x + p[v1].y * p[v1].y;
+	int d2 = p[v2].x * p[v2].x + p[v2].y * p[v2].y;
+	return d1 < d2;
+}
+bool first(int v)
+{
+	if(p[v].x*p[v].x + p[v].y * p[v].y < (7.5+D) * (7.5+D))
 		return true;
-	}
 	return false;
 }
 bool reach(int v)
 {
-	if(p[v].x + D > 50 || p[v].x - D <-50 || p[v].y + D > 50 || p[v].y - D <-50)
+	if(p[v].x +D >= 50 || p[v].x - D <= -50 || p[v].y + D >= 50 || p[v].y-D <= -50)
+		return true;
+	return false;
+}
+bool connect(int v1,int v2)
+{
+	int dx = (p[v1].x - p[v2].x);
+	int dy = (p[v1].y - p[v2].y);
+	if(dx*dx + dy*dy <= D*D )
 	{
 		return true;
 	}
 	return false;
 }
-void first_step()
+void BFS()
 {
-	for(int i = 0; i < p.size(); i++)
-	{
-		if( p[i].x*p[i].x + p[i].y*p[i].y < (7.5+D)*(7.5+D) )
-			dist[i] = 1;
-		else
-			dist[i] = -1;
-	}
-}
-void output_path(int v)
-{
-	Path.push_back(v);
-	if(path[v] != -1)
-	{
-		output_path(path[v]);
-	}
-}
-void Unweight()
-{
-	first_step();
+	vector<int> s;
 	vector<int> q;
-	for(int i = 0; i < N;i++)
+	vector<bool> visited(MAX,false);
+	for(int i = 0; i < p.size();i++)
 	{
-		if(reach(i))
-			r.push_back(i);
-		if(dist[i] != -1)
-			q.push_back(i);	
+		s.push_back(i);
 	}
-	while(!q.empty())
+	sort(s.begin(),s.end(),cmp);
+	for(int i = 0; i < s.size();i++)
 	{
-		int V = q[0];
-		q.erase(q.begin());
-		for(int i = 0; i < p.size();i++)
+		if(first(s[i]))
 		{
-			if(connect(V,i))
-			{
-				if(dist[i] == -1)
-				{
-					dist[i]  = dist[V] + 1;
-					path[i] = V;
-					q.push_back(i);
-				}
-			}
+			q.push_back(s[i]);
+			visited[s[i]] = true;
 		}
 	}
+	int step = 2;
+	int tail;
+	if(!q.empty())
+	 tail = q[q.size()-1];
+	while(!q.empty())
+	{
+			int top = q[0];
+			q.erase(q.begin());
+			if(reach(top))
+			{
+				cout << step << endl;
+				vector<int> Path;
+				while(top != -1)
+				{
+					Path.push_back(top);
+					top = path[top];
+				}
+				for(int i = Path.size()-1;i >= 0; i--)
+				{
+					cout << p[Path[i]].x << ' '<< p[Path[i]].y << endl;
+				}
+				return ;
+			}
+			for(int i = 0; i < p.size();i++)
+			{
+				if(connect(top,i) && !visited[i])
+				{
+					q.push_back(i);
+					visited[i] = true;
+					path[i] = top;
+				}		
+			}
+			if(top == tail)
+			{
+				step ++;
+				tail = q[q.size()-1];
+			}
+	}
+	if(q.empty())
+	cout << 0 << endl;
 }
 int main()
 {
-	cin >> N >> D;
-	for(int i = 0; i < N; i++)
+	cin >> N >> D;	
+	for(int i = 0; i < N;i++)
 	{
 		int a,b; cin >> a >> b;
-		if( abs(a) > 50 || abs(b) > 50 || a*a+b*b <= 7.5*7.5)
-			continue;
-		node t;
-		t.x = a; t.y = b;
+		
+		node t; t.x = a; t.y  = b;
 		p.push_back(t);
 		path[i] = -1;
 	}
-	sort(p.begin(),p.end());
-	if(D + 7.5 > 50)
+	if(D + 7.5 >= 50)
 	{
-		cout << 1 << endl;
-		return 0;
+		cout << 1; return 0;
 	}
-	Unweight();
-	int min_dist = 999999;
-	int num = -1;
-	for(auto &i : r)
-	{
-		if(dist[i] != -1 && dist[i] < min_dist)
-		{
-			min_dist = dist[i];
-			num = i;
-		}
-	}
-	if(num == -1)
-	{
-		cout << 0; return 0;
-	}
-	else
-	{
-		cout << min_dist << endl;
-		output_path(num);
-		for(auto i = Path.rbegin(); i != Path.rend()-1;i++)
-		{
-			cout << p[*i].x <<' '<< p[*i].y << endl;
-		}
-	}
+	BFS();
 }
