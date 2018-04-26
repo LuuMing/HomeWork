9ae07b6e1b403e535d31491c434a259a545337d1
LuMing <784315443@qq.com>
2018-04-26 12:54:16 +0800

    Pintia 公路村村通(30). Kruskal算法，求最小生成树并判断是否连通. 配合使用查并集完成Kruskal，简单快捷。

---
+++ c++/公路村村通.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#define MAX 3001
+using namespace std;
+int N;
+int M;
+int pre[MAX];
+struct edge
+{
+	int u;
+	int v;
+	int w;
+};
+vector<edge> e;
+bool cmp(const edge &e1, const edge & e2)
+{
+	return e1.w < e2.w;
+}
+int find (int v1){return (pre[v1]==v1)?v1:pre[v1] = find(pre[v1]);}
+int Kruskal()
+{
+	for(int i = 0; i < N;i++)	pre[i] = i;
+	sort(e.begin(),e.end(),cmp);
+	int total = 0;
+	for(int i = 0; i < e.size();i++)
+	{
+		int a = find(e[i].v); int b = find(e[i].u);
+		if(a!=b)
+		{
+			pre[a] = b;
+			total += e[i].w;
+		}
+	}
+	int comp = 0;
+	for(int i = 0; i < N;i++)
+	{
+		if(pre[i] ==  i) comp++;
+	}
+	return (comp==1)?total:-1;
+}
+int main()
+{
+	cin >> N >> M;
+	e.resize(M);
+	for(int i = 0; i < M;i++)
+	{
+		cin >> e[i].u >> e[i].v >> e[i].w;	
+	}
+	cout << Kruskal();
+}
