1a0bc7de7a94a1f253e2e803d414dbde83352d0d
LuMing <784315443@qq.com>
2018-04-30 22:03:53 +0800

    PAT 1039.Course List for Student(25). Hash for string.(c11's will run out of time) similar to polynomial calculate. int hash_str(char s[]) { 	int id = 0; 	for(int i = 0; i < n;i++) 		id = id * 26 + s[i]-'A'; 	return id; } hard:★★☆☆☆ time:★★★★☆

--- c++/PAT/Course_List_for_Student.cpp
+++ c++/PAT/Course_List_for_Student.cpp
@@ -1,42 +1,48 @@
 #include<iostream>
-#include<algorithm>
-#include<vector>
 #include<cstdio>
-#define MAX 40008
-#include<unordered_map>
+#include<vector>
+#include<algorithm>
+#define MAX 26*26*26*10+1
 using namespace std;
 int N;
 int K;
-unordered_map<string,int> str2int;
-vector<int> person[MAX];
-int main()
+vector<int> c[MAX];
+int hash_id(char name[])
 {
 	int id = 0;
+	for(int i = 0 ; i < 3; i++)
+	{
+		id = id * 26 + (name[i] -'A');
+	}
+	id = id * 10 + (name[3]-'0');
+	return id;
+}
+int main()
+{
 	cin >> N >> K;
+	char name[5];
 	for(int i = 0; i < K;i++)
 	{
-		int c,n; scanf("%d%d",&c,&n);
-		for(int i = 0; i < n;i++)
+		int cc,n; scanf("%d%d",&cc,&n);
+		for(int i = 0; i < n; i++)
 		{
-			string name;cin >> name;
-			if(!str2int.count(name))
-			{
-				str2int[name] = ++id;
-			}
-			person[str2int[name]].push_back(c);
+			scanf("%s",name);
+			int id = hash_id(name);
+			c[id].push_back(cc);
 		}
 	}
-	for(int i = 0; i < N;i++)
+	for(int i = 0; i < N;i++ )
 	{
-		string t; cin >> t;
-		cout << t<<' ';
-		sort(person[str2int[t]].begin(),person[str2int[t]].end());
-		cout << person[str2int[t]].size();
-		for(int i =0; i < person[str2int[t]].size();i++)
+		scanf("%s",name);
+		int id = hash_id(name);
+		printf("%s ",name);
+		int n = c[id].size();
+		printf("%d",n);
+		sort(c[id].begin(),c[id].end());
+		for(int i = 0; i < c[id].size();i++)
 		{
-			cout <<' '<<person[str2int[t]][i];
-		
+			printf(" %d",c[id][i]);
 		}
-		cout << endl;
+		printf("\n");
 	}
 }
