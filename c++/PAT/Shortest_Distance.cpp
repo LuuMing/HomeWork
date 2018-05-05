ff4f1e64a0febc16ae051ca33d33d18a27eb442b
LuMing <784315443@qq.com>
2018-05-05 09:24:39 +0800

    PAT 1046.Shortest Distance. Calculate the sum of the number on a curcular circle. 3 method: 1. sum over the point a to point b. And 1 to point a, b to point N. (Time exceed) 2. sum over the point a to point b. Total minus a to b will get another distance. (Time exceed, O(2N)) 3. store the dist[i], that mean the distance between point i and point 1. use dist[b-1] - dist[a-1] will get first distance. Total-(dist[b-1]-dist[a-1]) will get another distance (O(N)). hard:★★★★☆ time:★★★★★

--- c++/PAT/Shortest_Distance.cpp
+++ c++/PAT/Shortest_Distance.cpp
@@ -1,34 +1,26 @@
 #include<iostream>
 #define MAX 100003
 using namespace std;
-int num[MAX];
+int dist[MAX];
 int N;
 int M;
 int sum;
-int dist(int a,int b)
-{
-	int right = 0;
-	if(a > b)
-		swap(a,b);
-	for(int i = a; i<b;i++)
-	{
-		right += num[i];
-	}
-	int left = sum - right;
-	return min(left,right);
-}
 int main()
 {
 	cin >> N;
-	for(int i = 1; i <= N; i++)
+	for(int i = 1; i <= N;i++)
 	{
-		scanf("%d",&num[i]);
-		sum += num[i];
+		int t;
+		scanf("%d",&t);
+		sum += t;
+		dist[i] = sum;
 	}
 	cin >> M;
-	for(int i = 0; i < M; i++)
+	for(int i = 0; i < M;i++)
 	{
-		int a,b; scanf("%d%d",&a,&b);
-		printf("%d\n",dist(a,b));
+		int a,b; cin >> a >>b;
+		if(a > b)
+		swap(a,b);
+		printf("%d\n",min(dist[b-1]-dist[a-1],sum-(dist[b-1]-dist[a-1])));
 	}
 }
