49018fe873b75041b3c595ece905a241cb1d91c5
LuMing <784315443@qq.com>
2018-06-23 11:23:46 +0800

    pthread creation example and Makefile written

---
+++ operating_system/pthread_example/pthread_example.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<pthread.h>
+void * foo(void * arg) /* thread main */
+{
+	printf("Hello World from thread!\n");
+	pthread_exit(NULL);
+}
+int main(void)
+{
+	int i;
+	pthread_t tid;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+	pthread_attr_setscope(&attr,PTHREAD_SCOPE_SYSTEM);
+	pthread_create(&tid,&attr,foo,NULL);
+	return 0;
+}
