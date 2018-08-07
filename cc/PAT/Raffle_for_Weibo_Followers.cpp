d71dfd5753d69f29560b301ec1b826a67996b165
LuMing <784315443@qq.com>
2018-08-07 20:39:37 +0800

    PTA 1124. Raffle for Weibo Followers (?) logic judge, similar to ranking the list. hard:★★☆☆☆ time:★★☆☆☆

---
+++ cc/PAT/Raffle_for_Weibo_Followers.cpp
@@ -0,0 +1,42 @@
+/*************************
+	@date:18/8/7 20:13 ~ 20:37
+	@author: LuMing
+*************************/
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+unordered_set<string>s;
+int M,N,S;
+int main()
+{
+	cin >> M >> N >> S;
+	char name[22];
+	bool has = false;
+	int num = 1;
+	for(int i = 1; i <= M; i++)
+	{
+		scanf("%s",name);
+		if(num< S)
+		{
+			continue; num++;
+		}
+		if(s.count(name))
+		{
+			continue;
+		}
+		else
+		{
+			num++;
+			if( (num-S) % N == 0)
+			{
+				printf("%s\n",name);
+				s.insert(name);
+				has = true;
+			}
+		}
+	}
+	if( !has)
+		printf("Keep going...\n");
+
+
+}
