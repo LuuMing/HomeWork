9f05915d7647326059d7e1bd6d4bf3119504da8f
LuMing <784315443@qq.com>
2018-08-10 15:07:51 +0800

    PTA 1117 Eddington Number (25) fix error test case. By using sort to reorder the miles E. Then, scan the whole sequence. The number of days that its miles greater than current day. Then output it. If the sequence was sorted, N-i stand for the days that its miles greater than days i. hard:★★☆☆☆ time:★★☆☆ skill:★★★★☆

--- cc/PAT/Eddington_Number.cpp
+++ cc/PAT/Eddington_Number.cpp
@@ -1,32 +1,29 @@
 /**************************
 	@author:LuMing
-	@date:18/8/10 14:17 ~ 14:39
+	@date:18/8/10 ???~15:07
 **************************/
 #include<iostream>
-#include<map>
+#include<vector>
+#include<algorithm>
 using namespace std;
-map<int,int>m;
-int main()
+int E[ 100001];
+int N;
+int main() 
 {
 	int N; cin >> N;
-	int max_m = -1;
 	for(int i = 0; i < N;i++)
 	{	
-		int t; scanf("%d",&t);
-		if(t > max_m)
-			max_m = t;
-		for(int j = t-1; j >= 0; j--)
-		{
-			m[j]++;
-		}
+		scanf("%d",E+i);
 	}
-	int e = -1;
-	for(auto i = m.rbegin(); i != m.rend(); i++)
+	sort(E,E+N);
+	int ans  = 0;
+	for(int i = 0; i < N ;i++)
 	{
-		if( i -> second >= i -> first)
+		if(E[i] > N - i)
 		{
-			cout << i->first;
-			break;		
+			ans = N-i;
+			break; 
 		}
 	}
+	cout << ans;
 }
