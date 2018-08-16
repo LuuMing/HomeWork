da12ecb31377f4a51b558f20cdd260f1b7080588
LuMing <784315443@qq.com>
2018-08-16 17:57:52 +0800

    PTA. 1114 Family Property (25) fix bug: "fabs(r1-r2)", not "fabs(r1) - fabs(r2)" Use dfs to list component and calculate the family data. hard:★★★☆☆ time:★★★★★

--- cc/PAT/Family_Property.cpp
+++ cc/PAT/Family_Property.cpp
@@ -1,20 +1,20 @@
+/*******************
+	@author: LuMing
+	@date: 18/8/16 12:31~13:17 modify:17:44
+*********************/
 #include<iostream>
 #include<math.h>
 #include<vector>
 #include<algorithm>
+#define MAX 10990
 using namespace std;
-#define MAX 10001
-int adj[MAX][MAX];
-int sets[MAX];
-int area[MAX];
-int visit[MAX];
-int N;
-int min_id = 9999;
-int total_k;
-int total_area;
-int total_sets;
-int max_id;
-vector<int> v;
+bool visit[MAX];
+struct family
+{
+	int sets;
+	int area;
+	vector<int> child;
+};
 struct result
 {
 	int min_id;
@@ -22,85 +22,92 @@ struct result
 	double avg_sets;
 	double avg_area;
 };
-void dfs(int id)
-{
-	visit[id] = 1;
-	if(id < min_id)
-		min_id = id;
-	total_k++;
-	total_area += area[id];
-	total_sets += sets[id];
-	for(int i = 0 ; i <= max_id;i++)
-	{
-		if(adj[id][i] && !visit[i])
-		{
-			dfs(i);
-		}
-	}
-}
-
+int N;
+vector<int> v;
 vector<result> r;
+family f[MAX];
+int total_k = 0;
+int total_sets = 0;
+int total_area = 0;
+int min_id = 99999;
 bool cmp(const result & r1, const result & r2)
 {
-	if(fabs(r1.avg_area - r2.avg_area) > 0.001)
-	{
-		return r1.avg_area > r2.avg_area;
-	}
-	else
+	double e = r1.avg_area - r2.avg_area;
+	if(fabs(e) < 0.0001)
 	{
 		return r1.min_id < r2.min_id;
+	}
+	else
+		return r1.avg_area > r2.avg_area;
+}
+void dfs(int id)
+{
+	visit[id] =  true;
+	total_k ++;
+	total_sets += f[id].sets;
+	total_area += f[id].area;
+	if(id < min_id)
+	{
+		min_id = id;
+	}
+	for(int i = 0; i < f[id].child.size(); i++)
+	{
+		if( !visit[f[id].child[i]])
+		{
+			dfs(f[id].child[i]);
+		}
 	}
 }
 int main()
 {
 	cin >> N;
-	for(int i = 0 ; i < N; i++)	
+	for(int i = 0; i < N; i ++)
 	{
-		int id,fa,ma,k;
+		int id,fa,ma,k,sets,area;
 		scanf("%d%d%d%d",&id,&fa,&ma,&k);
-		if(id > max_id)
+		if(fa != -1)
 		{
-			max_id = id;
+			f[id].child.push_back(fa);
+			f[fa].child.push_back(id);
 		}
-	
+		if(ma != -1)
+		{
+			f[id].child.push_back(ma);
+			f[ma].child.push_back(id);
+		}
 		for(int j = 0; j < k;j++)
 		{
 			int t; scanf("%d",&t);
-			adj[id][t] = adj[t][id] = 1;
+			f[t].child.push_back(id);
+			f[id].child.push_back(t);
 		}
-		if( fa != -1)	
-		{
-			adj[fa][id] = adj[id][fa]  =1;
-		}
-		if( ma  != -1)
-		{
-			adj[ma][id] = adj[id][ma] = 1;
-		}
-		scanf("%d%d",sets+id,area+id);
+		scanf("%d%d",&sets,&area);
+		f[id].sets = sets;
+		f[id].area = area;
 		v.push_back(id);
 	}
-	for(int i = 0; i <v.size(); i++)
+	for(int i = 0; i < v.size() ;i++)
 	{
 		total_k = 0;
-		min_id = 9999;
 		total_sets = 0;
 		total_area = 0;
+		min_id = 999999;	
 		if(!visit[v[i]])
 		{
 			dfs(v[i]);
-			result t;
-			t.min_id = min_id;
-			t.k = total_k;
-			t.avg_area = (double)total_area / (double) total_k;
-			t.avg_sets = (double) total_sets / (double) total_k;
-			r.push_back(t);
 		}
+		else
+			continue;
+		result t; t.min_id = min_id;
+		t.k = total_k;
+		t.avg_sets = (double)total_sets / (double)total_k;
+		t.avg_area = (double)total_area / (double)total_k; 
+		r.push_back(t);
 	}
-	cout << r.size() << endl;
 	sort(r.begin(),r.end(),cmp);
-	for(int i = 0; i < r.size();i++)
+	cout << r.size() << endl;
+	for(int i = 0; i < r.size(); i++)
 	{
 		printf("%04d %d %.3f %.3f\n",r[i].min_id,r[i].k,r[i].avg_sets,r[i].avg_area);
 	}
-	
 }
