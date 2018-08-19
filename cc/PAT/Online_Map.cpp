bff0b387e02ca066838eef75787085f9fe612837
LuMing <784315443@qq.com>
2018-08-19 22:16:09 +0800

    PTA 1111 Online Map (23).[WA] Use pre_num[MAX] array to record the number of passed vertex of vertex. One test case error.

--- cc/PAT/Online_Map.cpp
+++ cc/PAT/Online_Map.cpp
@@ -1,183 +1,170 @@
-/***************************
+/*****************
 	@author: LuMing
-	@date:	18/8/18 12:49~ 13:05
-		13:12~13:59
-***************************/
+	@date: 18/8/19 20:27~22:15
+*****************/
 #include<iostream>
 #include<vector>
+using namespace std;
 #define MAX 501
 #define INF 99999
-using namespace std;
+int map[MAX][MAX];
+vector<int> v1,v2;
 int length[MAX][MAX];
 int Time[MAX][MAX];
-int pre_path[MAX];
-int pre_time[MAX];	
+int N, M;
+int pre_length[MAX];
+int pre_Time[MAX];
+int S,E;
 int D[MAX];
 int T[MAX];
-int N;
-int S, E;
-vector<int> v1,v2;
-void output_path(int u)
+int visit[MAX];
+void output1(int v)
 {
-	if(u == pre_path[u])
-	{
-		v1.push_back(u);
-		return;
-	}
+	v1.push_back(v);
+	if(v == pre_length[v])
+		return;	
 	else
-	{
-		v1.push_back(u);
-		output_path(pre_path[u]);
-	}
+		output1(pre_length[v]);
 }
-void output_time(int u)
+void output2(int v)
 {
-	if(u == pre_path[u])
-	{
-		v2.push_back(u);
-		return;
-	}
+	v2.push_back(v);
+	if(v == pre_Time[v])
+		return;	
 	else
-	{
-		v2.push_back(u);
-		output_time(pre_time[u]);
-	}
-}
-void dijkstra(int v)
-{
-	int visit[MAX];	
-	for(int i = 0; i < N;i++)
-	{	
-		visit[i] = 0;
-		D[i] = length[v][i];
-		T[i] = Time[v][i];
-		pre_path[i] = i;
-	}
-	visit[v] = 1;
-	for(int i = 0; i < N ;i++)
-	{
-		int num = -1;
-		int dist = INF;
-		for(int j = 0; j < N; j++)
-		{
-			if( D[j] < dist && !visit[j])
-			{
-				num = j;
-				dist = D[j];
-			}
-		}
-		if(num == -1)
-			break;
-		visit[num] = 1;
-		for(int u = 0; u < N; u++)
-		{
-			if( D[num] + length[num][u]  <  D[u] && !visit[u])
-			{
-				pre_path[u] = num;
-				D[u] = D[num] + length[num][u];
-				T[u] = T[num] +Time[num][u];
-			}
-			else if (D[num] + length[num][u] == D[u] && !visit[u] && T[num] + Time[num][u] < T[u])
-			{
-				pre_path[u] = num;
-				D[u] = D[num] + length[num][u];
-				T[u] = T[num] + Time[num][u];
-			}
-		}
-	}
-	output_path(E);
-	int DD = D[E];
-//////////////////////////////////////
-	for(int i = 0; i < N;i++)
-	{	
-		visit[i] = 0;
-		D[i] = length[v][i];
-		T[i] = Time[v][i];
-		pre_time[i] = i;
-	}
-	visit[v] = 1;
-	for(int i = 0; i < N ;i++)
-	{
-		int num = -1;
-		int dist = INF;
-		for(int j = 0; j < N; j++)
-		{
-			if( T[j] < dist && !visit[j])
-			{
-				num = j;
-				dist = T[j];
-			}
-		}
-		if(num == -1)
-			break;
-		visit[num] = 1;
-		for(int u = 0; u < N; u++)
-		{
-			if( T[num] + Time[num][u]  <  T[u] && !visit[u])
-			{
-				pre_time[u] = num;
-				D[u] = D[num] + length[num][u];
-				T[u] = T[num] +Time[num][u];
-			}
-			else if (T[num] + Time[num][u] == T[u] && !visit[u] &&    D[num] + length[num][u] < D[u])
-			{
-				pre_time[u] = num;
-				D[u] = D[num] + length[num][u];
-				T[u] = T[num] + Time[num][u];
-			}
-		}
-	}
-	output_time(E);
-	if(v1 == v2)
-	{
-			cout << "Distance = " << DD <<"; ";
-	}
-	else
-	{
-		cout << "Distance = " << DD <<": "<<S<<" -> ";
-		for(int i = v1.size()-1; i >=0;i--)
-		{	
-			cout << v1[i];
-			if(i!=0)
-				cout << " -> ";
-		}
-		cout << endl;
-	}
-	cout << "Time = " << T[E] <<": "<<S<<" -> ";
-	for(int i = v2.size()-1; i >=0;i--)
-	{
-		cout << v2[i];
-		if(i!=0)
-			cout << " -> ";
-	}
-	cout << endl;
+		output2(pre_Time[v]);
 }
 int main()
 {
-	int M;cin >> N >> M;
-	for(int i = 0; i < N ;i++)
+	int Dist;
+	cin >> N >> M;
+	for(int i = 0; i < N;i++)
 	{
-		for(int j = 0; j < N; j++)
+		pre_length[i] = i;
+		pre_Time[i] = i;
+		for(int j = 0; j < N;j++)
 		{
-			length[i][j] = Time[i][j] = (i==j)?0:INF;
+			length[i][j] = length[j][i] = (i==j)?0:INF;
+			Time[i][j] = Time[j][i] = (i==j)?0:INF;
 		}
 	}
-	for(int i = 0; i < M; i++)
+	for(int i = 0; i < M;i++)
 	{
 		int v1,v2,one_way,l,t;
 		scanf("%d%d%d%d%d",&v1,&v2,&one_way,&l,&t);
 		if(one_way)
 		{
+			map[v1][v2] = 1;
 			length[v1][v2] = l;
 			Time[v1][v2] = t;
 		}
 		else
 		{
+			map[v1][v2] = 1;
+			map[v2][v1] = 1; 
 			length[v1][v2] = length[v2][v1] = l;
 			Time[v1][v2] = Time[v2][v1] = t;
 		}
 	}
 	cin >> S >> E;
-	dijkstra(S);
-	return 0;
+	for(int i = 0; i < N;i++)
+	{
+		D[i] = length[S][i];
+		T[i] = Time[S][i];
+	}
+	visit[S] = 1;
+	for(int i = 0; i < N;i++)
+	{
+		int num = -1;
+		int min_dist = 99999;
+		for(int j = 0; j < N; j++)
+		{
+			if(!visit[j] && D[j] < min_dist)
+			{
+				min_dist = D[j];
+				num = j;
+			}
+		}
+		if(num == -1)
+			break;
+		visit[num] = 1;
+		for(int v = 0; v < N; v++)
+		{
+			if(!visit[v] && length[num][v] != INF)
+				if(D[num] + length[num][v] < D[v] && )
+				{
+					T[v] = T[num] + Time[num][v];
+					D[v] = D[num] + length[num][v];	
+					pre_length[v] = num;
+				}
+				else if (D[num] + length[num][v] == D[v]  &&
+					T[num]+ Time[num][v] < T[v] && map[num][v])
+				{
+					T[v] = T[num] + Time[num][v];
+					pre_length[v] = num;
+				}
+		}
+	}
+	output1(E);
+	Dist = D[E];
+	int pre_num[MAX];
+	for(int i = 0; i < N;i++)
+	{
+		D[i] = length[S][i];
+		T[i] = Time[S][i];
+		visit[i] = 0;
+		pre_num[i] = 0;
+	}
+	visit[S] = 1;
+	for(int i = 0; i < N;i++)
+	{
+		int num = -1;
+		int min_dist = 99999;
+		for(int j = 0; j < N; j++)
+		{
+			if(!visit[j] && D[j] < min_dist)
+			{
+				min_dist = D[j];
+				num = j;
+			}
+		}
+		if(num == -1)
+			break;
+		visit[num] = 1;
+		for(int v = 0; v < N; v++)
+		{
+			if(!visit[v] && length[num][v] != INF)
+				if(T[num] + Time[num][v] < T[v])
+				{
+					T[v] = T[num] + Time[num][v];
+					pre_Time[v] = num;
+					pre_num[v] = pre_num[num] + 1;
+				}
+				else if (T[num] + Time[num][v] == T[v] && pre_num[num] + 1 < pre_num[v]) 
+				{
+					pre_Time[v] = num;
+					pre_num[v] = pre_num[num] + 1;
+				}
+			}
+		}
+	output2(E);
+	if( v1 == v2)
+	{
+		cout << "Distance = " << Dist <<"; ";
+	}
+	else
+	{
+		cout << "Distance = " << Dist <<": "<<S;
+		for(auto i = v1.rbegin();  i!=v1.rend(); i++)
+		{	
+			cout << " -> " <<*i;
+		}
+		cout << endl;
+		
+	}
+	cout << "Time = " <<  T[E] <<": "<<S;
+	for(auto i = v2.rbegin();  i!=v2.rend(); i++)
+	{	
+		cout << " -> " <<*i;
+	}
 }
