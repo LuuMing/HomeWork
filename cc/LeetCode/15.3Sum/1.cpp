08ad17109a7cf0f5566d0a42534cb10f5c3a422e
LuMing <784315443@qq.com>
2018-09-10 20:27:39 +0800

    LeetCode 15.3Sum Given Array, find three numbers sum up equal to 0. These 3 numbers must be unique. ★ sort the array, use two pointer i,j. ★ i from current subscript plus 1, j from end. ★ slide i and j, moving  i,j based on num i plus num j 's value. ★ when find one, move i,j untile meet different number. skill:★★★★☆

--- cc/LeetCode/15.3Sum/1.cpp
+++ cc/LeetCode/15.3Sum/1.cpp
@@ -1,46 +1,40 @@
 class Solution {
-    set<set<int>> ss;
-    vector<vector<int>> result;
-    void find_2(int num,int idx, vector<int> & nums)
-    {
-        unordered_set<int> s;
-        vector<int> v;
-        v.push_back(num);
-        for(int i = 0; i < nums.size(); i++)
-        {
-            if(i == idx)
-                continue;
-            if( s.count(-num - nums[i]))
-            {
-                set<int> s;
-                s.insert(num);
-                s.insert(nums[i]);
-                s.insert(-num-nums[i]);
-                if(ss.count(s))
-                    continue;
-                ss.insert(s);
-                vector<int> vv = v;
-                vv.push_back(nums[i]);
-                vv.push_back(-num - nums[i]);
-                for(int j = 0; j < vv.size(); j++)
-                {
-                    cout << vv[j] <<' ';
-                }
-                cout << endl;
-                result.push_back(vv);
-            }
-            else
-            {
-                s.insert(nums[i]);
-            }
-        }
-         
-    }
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++)
-            find_2(nums[i],i,nums);
-        return result;
+  	sort(nums.begin(),nums.end());
+	vector<vector<int>> result;
+	for(int i = 0; i < nums.size(); i++)
+	{	
+        if (i >= 1) {
+            while (nums[i] == nums[i-1]) {
+                i++;
+                continue;
+            }
+        }
+
+		int s = i + 1, e = nums.size() - 1;
+		while( s < e)
+		{
+			if(nums[s] + nums[e] < -nums[i])
+			{
+				s++;
+			}
+			else if( nums[s] + nums[e] > -nums[i])
+			{	
+				e--;
+			}
+			else
+			{
+				vector<int>tmp = {nums[i],nums[s],nums[e]};
+				result.push_back(tmp);
+				while(s < e && nums[s] == nums[s+1])
+					s++;
+				while(s < e && nums[s] == nums[s+1])
+					e--;
+				s++,e--;
+			}
+		}
+	}   
+	return result;
     }
 };
-
