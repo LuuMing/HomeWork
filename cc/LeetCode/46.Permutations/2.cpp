78f6b216c3739d98d61f96f8a486b5accbf8443c
LuMing <784315443@qq.com>
2018-10-21 10:51:44 +0800

    LeetCode 46. Permutations 深搜，与 1.cpp 不同之处在于形参所传递的为非引用。 这样所导致两处区别： 	1. 空间复杂度提升，每次递归会额外生成一个 vector 	2. 在递归结束后没有“恢复现场”操作。即 swap 回原来的元素。

---
+++ cc/LeetCode/46.Permutations/2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    void dfs(int idx,vector<int>  nums)
+    {
+        if( idx == nums.size() - 1)
+        result.push_back(nums);
+        for(int i = idx; i < nums.size(); i++)
+        {
+            swap(nums[idx],nums[i]);
+            dfs(idx + 1,nums);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        dfs(0,nums);
+        return result;
+    }
+};
