abefc013a275199647b5b5419c7625da59828618
LuMing <784315443@qq.com>
2018-12-27 19:59:47 +0800

    LeetCode 171. Excel Sheet Column Number
    将 excel 的字母序列转换成数字。
    'A' ->  1,'AB' -> 28
    十进制转 25 进制。做吐了。

---
+++ cc/LeetCode/171.Excel_Sheet_Column_Number/1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int result = 0;
+        for( auto & c: s )
+        {
+            result = result * 26 + (c - 'A' + 1);
+        }
+        return result;
+    }
+};
