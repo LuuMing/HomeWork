f29715cfa2721026d137fe75d9260f86773b23d4
LuMing <784315443@qq.com>
2019-02-12 17:59:10 +0800

    LeetCode 344. Reverse String 翻转一个字符串，要求用 o(1) 空间。 两头扫，互相交换即可。（偷懒）

---
+++ cc/LeetCode/344.Reverse_String/1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int lo = 0, hi = s.size() - 1;
+        while( lo < hi)
+        {
+            swap( s[lo], s[hi] );
+            lo ++, hi --;
+        }
+    }
+};
